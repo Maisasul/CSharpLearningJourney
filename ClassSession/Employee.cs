fae6faf785de83ceac9e92a4bebea09c407b528e
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-05-15 15:36:09 +0400

    Add files via upload

---
+++ ClassSession/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSession
+{
+    public class Employee
+    {
+        public const double TAX = 0.03;
+        public string firstName;
+        public string lastName;
+        public double wage;
+        public double loggeHour;
+        public int Saa(int x)
+        {
+            return x + 1;
+        }
+    }
+    
+}
---
+++ ClassSession/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassSession
+{
+    //public class Employee
+    //{
+      //  public const double TAX = 0.03; 
+      //  public string firstName;
+     //   public string lastName;
+    //    public double wage;
+     //   public double loggeHour;
+   // } 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee employee = new Employee();
+            
+            /*employee.firstName = "Maisa";
+            employee.lastName = "Sul";
+            employee.wage = 5;
+            employee.loggeHour = 10;*/
+
+            Console.WriteLine("Enter number of employee : "); //for loop run based on user input
+            int munOfEmp = Convert.ToInt32(Console.ReadLine());
+            //int[] emps  = new Employee(munOfEmp);
+            for (int i = 0; i <= munOfEmp; i++)
+            {
+            Console.WriteLine($"Enter Your Full name:");
+            String firstName = Console.ReadLine();
+            Console.WriteLine($"Enter the wage:");
+            double wage = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Enter the logged Hours:");
+            double loggeHour = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Your Full name:{employee.firstName} {employee.lastName}");
+            // calculation
+            double totalSalary = employee.loggeHour * employee.wage;
+            double taxRate = totalSalary * Employee.TAX;
+            double netSalary = totalSalary - taxRate;
+
+            // user output of calculaton
+            Console.WriteLine($"Total salary: {totalSalary}");
+            Console.WriteLine($"Tax Rate: {taxRate}");
+            Console.WriteLine($"Net Salary: {netSalary}");
+
+
+
+            }
+            
+        }
+    }
+}
