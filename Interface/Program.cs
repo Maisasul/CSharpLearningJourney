67454291994a41c252416129178aeaf74150713d
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-05-20 18:03:34 +0400

    interface

---
+++ Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Honda("Super", 2014, "Car");
+            Honda honda = new Honda("Red", 2023, "Car");
+            honda.Ready();
+            honda.Start();
+            honda.Stop();
+
+            Caterpilar caterpilar = new Caterpilar("LALA", 1899, "Big");
+            caterpilar.Ready();
+            //  caterpilar.Load();
+
+            ILoader loader = new Caterpilar("NO", 1999, "No");
+            loader.Start();
+
+        }
+    }
+}
---
+++ Interface/Vehicle.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    abstract class Vehicle
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Category { get; set; }
+
+        public Vehicle(string Model, int Year, string Category) 
+        {
+            this.Model = Model;
+            this.Year = Year;
+            this.Category = Category;
+        }
+    }
+
+    interface IDrivoble
+    {
+        void Ready();
+        void Start();
+        void Stop();
+    }
+
+    interface ILoader
+    {
+        void Load();
+        void UnLoad();
+        void Start();
+    }
+
+    class Honda : Vehicle, IDrivoble
+    {
+        public Honda(string Model, int Year, string Category) : base(Model, Year, Category)
+        {
+        }
+        
+        public void Ready() 
+        {
+            Console.WriteLine("Ready To Move!");
+        }
+
+        public void Start() 
+        {
+            Console.WriteLine("Start Honda");
+        }
+
+        public void Stop() 
+        {
+            Console.WriteLine("Stop Honda");
+        }
+    }
+
+    class Caterpilar : Vehicle, IDrivoble, ILoader
+    {
+        public Caterpilar(string Model, int Year, string Category) : base (Model, Year, Category) 
+        {
+        }
+
+        void ILoader.Start()
+        {
+            Console.WriteLine("Start Loading");
+        }
+        void ILoader.Load() 
+        {
+            Console.WriteLine("Loading..");
+        }
+        void ILoader.UnLoad() 
+        {
+            Console.WriteLine("UnLoad..");
+        }
+        
+        void IDrivoble.Start() 
+        {
+            Console.WriteLine("Start Your Caterpilar");
+        }
+        public void Ready() 
+        {
+            Console.WriteLine("Get Ready");
+        }
+        void IDrivoble.Stop() 
+        {
+            Console.WriteLine("STOP!!!");
+        }
+      
+    }
+}
