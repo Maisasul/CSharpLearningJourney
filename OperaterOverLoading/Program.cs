fbbebc8d617ffa0212403a6050804c5f0fb20a49
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-05-23 22:14:09 +0400

    Operator OverLodaing

---
+++ OperaterOverLoading/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorOverLoading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operator Over Loading");
+            Box box1 = new Box(8);
+            Box box2 = new Box(2);
+            Box box3 = box1 + box2;
+            Box box4 = box1 - box2;
+            Box box5 = box1 / box2;
+            Box box6 = box1 * box2;
+            Console.WriteLine($"+ Operator {box3.waight}");
+            Console.WriteLine($"- Operator {box4.waight}");
+            Console.WriteLine($"/ Operator {box5.waight}");
+            Console.WriteLine($"* Operator {box6.waight}");
+        }
+    }
+    class Box
+    {
+        public decimal waight { get; set; }
+
+        public Box(decimal waight)
+        {
+            this.waight = waight;
+        }
+        public static Box operator +(Box a, Box b)
+        {
+            decimal c = a.waight + b.waight;
+            return new Box(c);
+        }
+        public static Box operator -(Box a, Box b) 
+        {
+            decimal c = a.waight - b.waight;
+            return new Box(c);
+        }
+        public static Box operator /(Box a, Box b)
+        {
+            decimal c = a.waight / b.waight;
+            return new Box(c);
+        }
+        public static Box operator *(Box a, Box b)
+        {
+            decimal c = a.waight * b.waight;
+            return new Box(c);
+        }
+        public static Box operator ==(Box a, Box b)
+        {
+            if (a.waight == b.waight)
+                return a;
+            return false;
+        }
+    }
+
+}
