9b902c48b103f8bd9ec9f4e24f9913cd5e2880ec
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-05-24 20:33:52 +0400

    the other and correct solution is on the way

---
+++ ClinicSystem/ClinicSystem/DAYINWEE.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicSystem
+{
+    internal class DAYINWEE
+    {
+        int day;
+        //enum day
+        //{
+        //  Sunday = 1,
+        //Monday,
+        //Tuesday,
+        //Wednesday,
+        //  Thursday,
+        //  Friday,
+        // Saturday,
+        // weekend = Friday & Saturday
+        // }
+
+        public string BookAppointment()
+        {
+            int day;
+            Console.WriteLine("Please choose a day?");
+            int dayChoose = Convert.ToInt32(Console.ReadLine());
+            
+            if (dayChoose == 6 || dayChoose == 7)
+            {
+                Console.WriteLine("It's weekend! no appointment. Please choose another day");
+            }
+            else if (dayChoose <= 5 && dayChoose >= 1)
+            {
+                Console.WriteLine($"Day selected is {this.day}");
+                Console.WriteLine("Your Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Relationship Status: ");
+                string relationship = Console.ReadLine();
+                Console.WriteLine("First visit? ");
+                string visit = Console.ReadLine();
+                Console.WriteLine("Confirmation reservation details:- ");
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Age: {age}");
+                Console.WriteLine($"Relationship Status: {relationship}");
+                Console.WriteLine($"First visit: { visit}");
+                Console.WriteLine($"appointment day: {this.day}");
+            }
+            switch (this.day)
+            {
+                case 1:
+                    //Console.WriteLine("Sunday"); 
+                    return"Sunday";
+                    break;
+                case 2:
+                    return "Monday";
+                    break;
+                case 3:
+                    return "Tuesday";
+                    break;
+                case 4:
+                    return "Wednesday";
+                    break;
+                case 5:
+                    return "Thursday";
+                    break;
+                case 6:
+                    return "Friday";
+                    break;
+                case 7:
+                    return "Saturday";
+                    break;
+                default:
+                    return "NO DAY";
+                    break;
+            }
+        }
+    }
+}
---
+++ ClinicSystem/ClinicSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClinicSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DAYINWEE dAYINWEE = new DAYINWEE();
+            Console.WriteLine("Welcome to the Clinic Reservation System!");
+            Console.WriteLine("Please select an option: ");
+            Console.WriteLine("1- Book an appointment.");
+            Console.WriteLine("2- Update an appointment.");
+            int mune = Convert.ToInt32(Console.ReadLine());
+            switch (mune)
+            {
+                case 1:
+                    dAYINWEE.BookAppointment();
+                    break;
+                case 2:
+                    Console.WriteLine("Update");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option, TRY AGAIN!");
+                    break;
+            }
+        }        
+    }
+}
