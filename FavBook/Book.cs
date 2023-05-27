1ab432efe0790d0422f170ec6689c3d8b4492661
Maisasul <104488163+Maisasul@users.noreply.github.com>
2023-05-27 16:13:44 +0400

    Indexer

---
+++ FavBook/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavBook
+{
+    internal class Book
+    {
+        private string[] BookName;
+        private string[] BookAuthor;
+        private int length;
+
+        public Book(int len) 
+        {
+            this.length = len;
+            this.BookName = new string[length];
+            this.BookAuthor = new string[length];
+        }
+
+        public string this[int indexer, string bookName]
+        {
+            get
+            {
+                string str = $"{bookName} : {BookAuthor[indexer]}";
+                return str;
+            }
+            set 
+            {
+                BookName[indexer] = bookName;
+                BookAuthor[indexer] = value;
+            }
+        }
+    }
+}
---
+++ FavBook/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FavBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book(6);
+            book[1, "Life is hard"] = "Amal";
+            book[2, "Deep space"] = "Maisa";
+            book[3, "Programer Brain"] = "Fatma";
+            Console.WriteLine(book[1,"Life is hard"]);
+            Console.WriteLine(book[2,"Deep space"]);
+            Console.WriteLine(book[3,"Programer Brain"]);
+        }
+    }
+}
